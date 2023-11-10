f9661196e3e36c6ee7f6cd49a5a7343192ef3f90
janealexadams <janealexadams@gmail.com>
2023-11-10 14:18:22 -0800

    added project animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,8 +17,8 @@ function Projects() {
       <div id="projects" className="text-center">
       <Container>
       <Row className="moodie">
-        <Col xs={6} md={6}>
-          <Image className="moodie-img" src={moodie} alt="Moodie" rounded />
+        <Col className="img-col" xs={6} md={6}>
+          <Image className="moodie-img " src={moodie} alt="Moodie" rounded />
         </Col>
         <Col xs={6} md={6}>
           <p className="desc">moodie project description</p>
@@ -28,12 +28,12 @@ function Projects() {
         <Col xs={6} md={6}>
           <p className="desc">techBlog project description</p>
         </Col>
-        <Col xs={6} md={6}>
+        <Col className="img-col" xs={6} md={6}>
         <Image className="tech-blog-img" src={techBlog} alt="tech-blog" rounded />
         </Col>
       </Row>
       <Row className="weather-dashboard">
-        <Col xs={6} md={6}>
+        <Col className="img-col" xs={6} md={6}>
           <Image className="weather-dashboard-img" src={weatherDashboard} alt="Weather Dashboard" rounded />
         </Col>
         <Col xs={6} md={6}>
@@ -44,12 +44,12 @@ function Projects() {
         <Col xs={6} md={6}>
           <p className="desc">note taker project description</p>
         </Col>
-        <Col xs={6} md={6}>
+        <Col className="img-col" xs={6} md={6}>
           <Image className="note-taker-img" src={noteTaker} alt="Note Taker" rounded />
         </Col>
       </Row>
       <Row className="code-quiz">
-        <Col xs={6} md={6}>
+        <Col className="img-col" xs={6} md={6}>
           <Image className="code-quiz-img" src={codeQuiz} alt="Code Quiz" rounded />
         </Col>
         <Col xs={6} md={6}>
