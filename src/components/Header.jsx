e25a71b6002839bb4331692882151b97ad44807c
janealexadams <janealexadams@gmail.com>
2024-01-08 14:41:29 -0800

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,12 +11,12 @@ function Header() {
     return (
         <section id="about-me">
             <Container className="header">
-                <Row className="header text-center">
+                <Row className="row text-center">
                     <Col className="text slide-in-left" xs={6} md={6}>
-                        <p className="title ">Hello, I’m Jane!</p>
+                        <p className="title">Hello, I’m Jane!</p>
                         <p classname="blurb">I’m a full-stack web developer based in Los Angeles, CA. I have a passion for all things technology and design - I enjoy creating applications that make a difference with a functional user experience.</p>
                     </Col>
-                    <Col xs={6} md={6}>
+                    <Col className="img-col" xs={6} md={6}>
                         <Image className="portrait slide-in-right" src={portrait} alt="self portrait" roundedCircle />
                     </Col>
                 </Row>
